75971c061a224e7d489ce7c1db1c26c8febb5b37
JamesJang-2jz <jhjx92@gmail.com>
2023-07-20 15:15:51 -0400

    square component receives value prop from board component

--- src/App.js
+++ src/App.js
@@ -1,37 +1,38 @@
 import React  from 'react';
 import { useState } from 'react';
 
-function Square() {
+function Square({value}) {
 /** components use STATE/useState to remember if it was clicked
     -useState declares two variables, first is a state variable that holds current value of the state.
     second value (setValue) is used by the useState hook to update the state variable.
 */
-    const [value, setValue] = useState(null);
-    function handleClick () {
-    setValue('X');
-    }
+//    const [value, setValue] = useState(null);
+//    function handleClick () {
+//    setValue('X');
+//    }
     return <button className="square" /* onClick={() =>{ alert('clicked'); }}*/
-    onClick={handleClick}>{value}</button>;
+    /*onClick={handleClick}*/
+    >{value}</button>;
 }
 
 export default function Board() {
-    const [square, setSquares] = useState(Array(9).fill(null));
+    const [squares, setSquares] = useState(Array(9).fill(null));
   return (
   <React.Fragment>
     <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[0]} />
+        <Square value={squares[1]} />
+        <Square value={squares[2]} />
     </div>
     <div className="board-row">
-            <Square />
-            <Square />
-            <Square />
+         <Square value={squares[3]}/>
+         <Square value={squares[4]}/>
+         <Square value={squares[5]}/>
     </div>
     <div className="board-row">
-            <Square />
-            <Square />
-            <Square />
+         <Square value={squares[6]}/>
+         <Square value={squares[7]}/>
+         <Square value={squares[8]}/>
     </div>
   </React.Fragment>
   );
